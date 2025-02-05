dc9fb46d3c6c8970df0dd6bb1bdee10c209e426d
nicollasprado <nicollasprado.profissional@gmail.com>
2025-02-04 21:00:34 -0300

    Questão 3 backtracking

--- Algoritmos/backtracking/lista01/q3.cpp
+++ Algoritmos/backtracking/lista01/q3.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <algorithm>
 
-int backtrack_qt_ways(int grid[6][6], int linha, int coluna, int l, int c){
+int backtrack_find(int grid[6][6], int linha, int coluna, int l, int c){
     // testes para ver se está no grid
-    if(l < 0 || c < 0 || l >= linha || c >= coluna || grid[l][c] != 0) return 0;
+    if(l < 0 || c < 0 || l >= linha || c >= coluna || grid[l][c] != 0) return 1000;
     // chegou no final
     if(l == linha-1 && c == coluna-1) return 1;
 
@@ -12,56 +12,20 @@ int backtrack_qt_ways(int grid[6][6], int linha, int coluna, int l, int c){
     grid[l][c] = 9;
 
     // soma de todas as possibilidades
-    int ans = 0;
-    ans += backtrack_qt_ways(grid, linha, coluna, l, c+1);
-    ans += backtrack_qt_ways(grid, linha, coluna, l+1, c);
-    ans += backtrack_qt_ways(grid, linha, coluna, l-1, c);
-    ans += backtrack_qt_ways(grid, linha, coluna, l, c-1);
-
+    int way_right = backtrack_find(grid, linha, coluna, l, c+1);
+    int way_down  = backtrack_find(grid, linha, coluna, l+1, c);
+    int way_up    = backtrack_find(grid, linha, coluna, l-1, c);
+    int way_left  = backtrack_find(grid, linha, coluna, l, c-1);
     grid[l][c] = 0;
-    return ans;
+
+    int min_way = std::min({way_right, way_up, way_down, way_left});
+    return (min_way == 1000) ? 1000 : min_way + 1;
 }
-
-int backtrack_find(int grid[6][6], int linha, int coluna, int l, int c, int* count){
-    // testes para ver se está no grid
-    if(l < 0 || c < 0 || l >= linha || c >= coluna || grid[l][c] != 0) return 0;
-    // chegou no final
-    if(l == linha-1 && c == coluna-1) return 1;
-
-    // marca por onde ja passou
-    grid[l][c] = 9;
-
-    // soma de todas as possibilidades
-    *count += backtrack_find(grid, linha, coluna, l, c+1, count) ||
-    backtrack_find(grid, linha, coluna, l+1, c, count) ||
-    backtrack_find(grid, linha, coluna, l-1, c, count) ||
-    backtrack_find(grid, linha, coluna, l, c-1, count);
-
-    grid[l][c] = 0;
-    return *count;
-}
-
 
 int backtrack(int grid[6][6], int linha, int coluna, int l, int c){
-    std::vector<int> paths;
-    int qt_ways = backtrack_qt_ways(grid, linha, coluna, 0, 0);
-
-    int ans = -1;
-    if(qt_ways > 0){
-        while((int) paths.size() != qt_ways-1){
-            int count = 0;
-            int count_path = backtrack_find(grid, linha, coluna, 0, 0, &count);
-            count = 0;
-            paths.push_back(count_path);
-        }
-
-        std::sort(paths.begin(), paths.end());
-        ans = paths[0];
-    }
-
-    return ans;
+    int result = backtrack_find(grid, linha, coluna, l, c);
+    return (result >= 1000) ? -1 : result;
 }
-
 
 
 
